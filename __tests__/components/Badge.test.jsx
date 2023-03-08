e9f365b77f5cd0e77f047d482787e7a183148639
Kudakwashe Mupeni <terrencemupeni@gmail.com>
2023-03-08 10:51:43 +0100

    chore: run prettier

--- __tests__/components/Badge.test.jsx
+++ __tests__/components/Badge.test.jsx
@@ -1,12 +1,13 @@
-import { render, screen, within } from '@testing-library/react';
-import { describe, expect, test } from 'vitest';
-
-import Badge, { defaultClassName } from 'components/Badge';
+import { render, screen, within } from '@testing-library/react'
+import Badge, { defaultClassName } from 'components/Badge'
+import { describe, expect, test } from 'vitest'
 
 describe('Badge', () => {
-	test('render with props', () => {
-		const { container } = render(<Badge className="testclass" text="basic" />);
-		expect(container.querySelector('.testclass').className).toBe(`${defaultClassName} testclass`);
-		expect(container.querySelector('.testclass').textContent).toBe('basic');
-	});
-});
+  test('render with props', () => {
+    const { container } = render(<Badge className="testclass" text="basic" />)
+    expect(container.querySelector('.testclass').className).toBe(
+      `${defaultClassName} testclass`
+    )
+    expect(container.querySelector('.testclass').textContent).toBe('basic')
+  })
+})
--- components/Table/InvestmentTable.jsx
+++ components/Table/InvestmentTable.jsx
@@ -1,66 +1,98 @@
-import Image from 'next/image';
+import NoDataTable from './NoDataTable'
+import { PencilIcon, TrashIcon } from '@heroicons/react/24/solid'
+import Table from 'components/Table'
+import { investmentCategory } from 'constants/index'
+import Image from 'next/image'
+import { sortByKey } from 'utils/array'
+import { formatCurrency, formatDate, isItToday } from 'utils/formatter'
 
-import { PencilIcon, TrashIcon } from '@heroicons/react/24/solid';
+const tdClassNames = 'relative p-4 pl-8 text-zinc-800 text-sm font-normal'
+const thList = [
+  'Name',
+  'Price',
+  'Units',
+  'Bought Date ↓',
+  'Category',
+  'Notes',
+  'Actions'
+]
 
-import Table from 'components/Table';
+export default function InvestmentTable({
+  isLoading,
+  data = [],
+  onEdit,
+  onDelete,
+  user,
+  onFilterChange,
+  filterKey
+}) {
+  const { currency, locale, isPremiumPlan, isPremiumPlanEnded } = user
 
-import { sortByKey } from 'utils/array';
-import { formatCurrency, formatDate, isItToday } from 'utils/formatter';
+  if (!isLoading && !data.length) {
+    return (
+      <NoDataTable
+        filterKey={filterKey}
+        isPremiumPlan={isPremiumPlan}
+        onFilterChange={onFilterChange}
+      >
+        <div className="flex flex-col items-center justify-center ">
+          <p className="mt-2 font-medium text-black sm:mt-10">
+            You don{"'"}t have any investment yet.
+          </p>
+          <Image
+            className="mt-2"
+            src="/static/illustrations/rich.svg"
+            width={300}
+            height={300}
+            alt="No investment"
+          />
+        </div>
+      </NoDataTable>
+    )
+  }
 
-import { investmentCategory } from 'constants/index';
-
-import NoDataTable from './NoDataTable';
-
-const tdClassNames = 'relative p-4 pl-8 text-zinc-800 text-sm font-normal';
-const thList = ['Name', 'Price', 'Units', 'Bought Date ↓', 'Category', 'Notes', 'Actions'];
-
-export default function InvestmentTable({ isLoading, data = [], onEdit, onDelete, user, onFilterChange, filterKey }) {
-	const { currency, locale, isPremiumPlan, isPremiumPlanEnded } = user;
-
-	if (!isLoading && !data.length) {
-		return (
-			<NoDataTable filterKey={filterKey} isPremiumPlan={isPremiumPlan} onFilterChange={onFilterChange}>
-				<div className="flex flex-col items-center justify-center ">
-					<p className="mt-2 font-medium text-black sm:mt-10">You don{"'"}t have any investment yet.</p>
-					<Image className="mt-2" src="/static/illustrations/rich.svg" width={300} height={300} alt="No investment" />
-				</div>
-			</NoDataTable>
-		);
-	}
-
-	return (
-		<Table
-			showFilter
-			onFilterChange={onFilterChange}
-			filterKey={filterKey}
-			title="Investments"
-			thList={thList}
-			isLoading={isLoading}
-			isPremiumPlan={isPremiumPlan && !isPremiumPlanEnded}
-		>
-			{sortByKey(data, 'date').map((datum) => {
-				const isToday = isItToday(new Date(datum.date), new Date());
-				return (
-					<tr key={datum.id} className="border-b border-gray-200 last:border-0 hover:bg-gray-50">
-						<td className={`${tdClassNames}`}>{datum.name}</td>
-						<td className={tdClassNames}>{formatCurrency(datum.price, currency, locale)}</td>
-						<td className={`${tdClassNames}`}>{datum.units}</td>
-						<td className={tdClassNames}>{isToday ? 'Today' : formatDate(datum.date, locale)}</td>
-						<td className={`${tdClassNames} capitalize`}>{investmentCategory[datum.category]}</td>
-						<td className={`${tdClassNames}  break-words`}>{datum.notes}</td>
-						<td className={`${tdClassNames}`}>
-							<div className="flex w-12 items-center justify-between">
-								<button onClick={() => onEdit(datum)} title="Edit">
-									<PencilIcon className="h-4 w-4 cursor-pointer text-slate-700 hover:text-slate-500" />
-								</button>
-								<button onClick={() => onDelete(datum.id)} title="Delete">
-									<TrashIcon className="h-4 w-4 cursor-pointer text-slate-700 hover:text-slate-500" />
-								</button>
-							</div>
-						</td>
-					</tr>
-				);
-			})}
-		</Table>
-	);
+  return (
+    <Table
+      showFilter
+      onFilterChange={onFilterChange}
+      filterKey={filterKey}
+      title="Investments"
+      thList={thList}
+      isLoading={isLoading}
+      isPremiumPlan={isPremiumPlan && !isPremiumPlanEnded}
+    >
+      {sortByKey(data, 'date').map((datum) => {
+        const isToday = isItToday(new Date(datum.date), new Date())
+        return (
+          <tr
+            key={datum.id}
+            className="border-b border-gray-200 last:border-0 hover:bg-gray-50"
+          >
+            <td className={`${tdClassNames}`}>{datum.name}</td>
+            <td className={tdClassNames}>
+              {formatCurrency(datum.price, currency, locale)}
+            </td>
+            <td className={`${tdClassNames}`}>{datum.units}</td>
+            <td className={tdClassNames}>
+              {isToday ? 'Today' : formatDate(datum.date, locale)}
+            </td>
+            <td className={`${tdClassNames} capitalize`}>
+              {investmentCategory[datum.category]}
+            </td>
+            <td className={`${tdClassNames}  break-words`}>{datum.notes}</td>
+            <td className={`${tdClassNames}`}>
+              <div className="flex w-12 items-center justify-between">
+                <button onClick={() => onEdit(datum)} title="Edit">
+                  <PencilIcon className="h-4 w-4 cursor-pointer text-slate-700 hover:text-slate-500" />
+                </button>
+                <button onClick={() => onDelete(datum.id)} title="Delete">
+                  <TrashIcon className="h-4 w-4 cursor-pointer text-slate-700 hover:text-slate-500" />
+                </button>
+              </div>
+            </td>
+          </tr>
+        )
+      })}
+    </Table>
+  )
 }
